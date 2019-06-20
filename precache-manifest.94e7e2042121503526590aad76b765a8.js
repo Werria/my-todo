bcf56f81411f2293b76f92a3d17dc90c33c55bee
Werria <verya.heydari@gmail.com>
2019-06-20 07:45:58 -0400

    Updates

---
+++ precache-manifest.94e7e2042121503526590aad76b765a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c1011f264757afa8ac0",
+    "url": "/my-todo/static/js/runtime~main.f98e7811.js"
+  },
+  {
+    "revision": "f44f2e7f1e6105008e59",
+    "url": "/my-todo/static/js/main.34d5cb93.chunk.js"
+  },
+  {
+    "revision": "7aa431cc78fc853e4124",
+    "url": "/my-todo/static/js/2.306dab95.chunk.js"
+  },
+  {
+    "revision": "f44f2e7f1e6105008e59",
+    "url": "/my-todo/static/css/main.3cf5369c.chunk.css"
+  },
+  {
+    "revision": "7bb6d490ecef62653651620bad5b83cf",
+    "url": "/my-todo/index.html"
+  }
+];
